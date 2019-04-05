c65ba3e2045649ebde20b4dfb18efb54e3379109
Daniel Oliveira <danielco94@gmail.com>
2019-04-05 17:56:29 +0100

    Move color functions to utils

--- src/stanford-diagram.js
+++ src/stanford-diagram.js
@@ -1,4 +1,4 @@
-import { range, scaleSequential, scaleLog } from './stanford-utils';
+import { range, scaleSequential, scaleLog, interpolateHSL } from './stanford-utils';
 
 /**
  * Get the stanfordPlugin options
@@ -258,66 +258,7 @@ CanvasRenderingContext2D.prototype.polygon = function (pointsArray, fillColor, s
   this.restore();
 };
 
-/**
- * Assumes h, s, and l are contained in the set [0, 1]
- * thanks to https://codepen.io/anon/pen/ZPqQdM
- *
- * @param a
- * @param b
- * @param o
- * @returns {String}
- */
-function interpolateHSL(a, b, o) {
-  const len = a.length;
-  const hsl = Array(len);
-
-  for (let i = 0; i < len; i++) {
-    const a1 = a[i];
-    hsl[i] = a1 + (b[i] - a1) * o;
-  }
-
-  return hslToRgb(hsl[0], hsl[1], hsl[2]);
-}
-
-/**
- * Converts an HSL color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   {number}  h       The hue
- * @param   {number}  s       The saturation
- * @param   {number}  l       The lightness
- * @return  {String}          The RGB representation
- */
-function hslToRgb(h, s, l) {
-  let r; let g; let
-    b;
-
-  if (s === 0) { // achromatic
-    r = l;
-    g = l;
-    b = l;
-  } else {
-    const hue2rgb = function hue2rgb(p, q, t) {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    };
-
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * l - q;
-
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
-  }
-
-  return `rgb(${Math.round(r * 255)}, ${Math.round(g * 255)}, ${Math.round(b * 255)})`;
-}
+// TODO https://www.chartjs.org/docs/latest/developers/charts.html
 
 /**
  * Stanford Diagram Plugin
